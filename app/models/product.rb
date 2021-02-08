e9848adea0fea84f495c498a5ba459723ebc325c
titanfat <tit22an@gmail.com>
2021-02-08 18:18:42 +0300

    Add point products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, :user_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 },
+            present: true
   belongs_to :user
 end
