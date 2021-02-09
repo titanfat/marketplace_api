ad68821d78c12a9a34caa9efcd16d51a0fdbb4c2
titanfat <tit22an@gmail.com>
2021-02-09 03:15:32 +0300

    Implemented search in product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::ProductsController < ApplicationController
   before_action :set_product, only: %i[show update destroy]
 
   def index
-    @products = Product.all
+    @products = Product.search(params)
     render json: ProductSerializer.new(@products).serializable_hash
   end
 
