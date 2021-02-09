e13a722f6bad11b498dfc729ab05a8b053dc2939
titanfat <tit22an@gmail.com>
2021-02-09 19:55:42 +0300

    Adds create methods order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,9 @@ class Order < ApplicationRecord
   has_many :products, through: :placements
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
+  before_validation :set_total!
+end
+
+def set_total!
+  self.total = products.map(&:price).sum
 end
