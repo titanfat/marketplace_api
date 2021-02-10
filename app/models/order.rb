02c6fa3e599c0c3a7784b1e795bd719eb42b80e2
titanfat <tit22an@gmail.com>
2021-02-10 06:40:01 +0300

    Create validator enough product on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
+  include ActiveModel::Validations
   belongs_to :user
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
   before_validation :set_total!
+  validates_with EnoughProductsValidator
 
   def set_total!
     self.total = products.map(&:price).sum
